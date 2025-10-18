e6f909cf8e5136f7cd661edd7a4670f07e131bfc
Crispy <tana1967gonchar@mail.ru>
2025-10-18 13:28:40 +0300

    Delete lab_4 directory

--- lab_4/bitonic_sortTest.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from bitonic_sort import bitonic_sort
-
-class TestSortingNetwork(unittest.TestCase):
-    def test_basic(self):
-        self.assertEqual(bitonic_sort([3, 7, 2, 5]), [2, 3, 5, 7])
-    def test_reverse(self):
-        self.assertEqual(bitonic_sort([9, 8, 7, 6]), [6, 7, 8, 9])
